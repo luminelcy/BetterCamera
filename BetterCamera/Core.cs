de9e27a8c0e618fb20d21af57c7f37a566951cbe
luminelcy <2323439368@qq.com>
2026-07-22 16:19:23 +0800

    添加项目文件。

---
+++ BetterCamera/Core.cs
@@ -0,0 +1,15 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(BetterCamera.Core), "BetterCamera", "1.0.0", "Author", null)]
+[assembly: MelonGame(null, null)]
+
+namespace BetterCamera
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+    }
+}
